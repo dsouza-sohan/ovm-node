9058c2aad5e1ea69b6c0e84eb91b568ca5e9a829
Marvin Delroy Aroza <developertom23@gmail.com>
2024-09-07 13:28:03 +0100

    app fix

--- Routes/Auth/login.js
+++ Routes/Auth/login.js
@@ -5,52 +5,52 @@ const User = require('../../Model/Auth/user');
 
 const jwttoken = require('jsonwebtoken');
 
-router.post('',async (req,res)=>{
-    try{
-        var userexists = await User.findOne({email:req.body.email, isActive:true});
+router.post('', async (req, res) => {
+    try {
+        var userexists = await User.findOne({ email: req.body.email, isActive: true });
 
-        if(!userexists){
+        if (!userexists) {
 
-            res.status(400).json({
-                code:400,
-                message:'Invalid Email!',
-                data:null,
-                status:false
+            return res.status(400).json({
+                code: 400,
+                message: 'Invalid Email!',
+                data: null,
+                status: false
             })
         }
 
-        var validatePass = await crypt.compare(req.body.password,userexists.password);
-        if(!validatePass){
-            res.status(400).json({
-                code:400,
-                message:'Invalid Password!',
-                data:null,
-                status:false
+        var validatePass = await crypt.compare(req.body.password, userexists.password);
+        if (!validatePass) {
+            return res.status(400).json({
+                code: 400,
+                message: 'Invalid Password!',
+                data: null,
+                status: false
             })
         }
 
-        var token = jwttoken.sign({_id:userexists._id,role:userexists.role},process.env.TOKEN_SECRET);
+        var token = jwttoken.sign({ _id: userexists._id, role: userexists.role }, process.env.TOKEN_SECRET);
 
         var data = {
-            id:userexists._id,
-            role:userexists.role,
-            firstname:userexists.firstname,
-            lastname:userexists.lastname,
-            profImage:userexists.profImage,
-            token:token
+            id: userexists._id,
+            role: userexists.role,
+            firstname: userexists.firstname,
+            lastname: userexists.lastname,
+            profImage: userexists.profImage,
+            token: token
         }
-        res.status(200).json({
-            code:200,
-            message:'Login successfully',
-            data:data,
-            status:true
+        return res.status(200).json({
+            code: 200,
+            message: 'Login successfully',
+            data: data,
+            status: true
         })
-    } catch(err){
-        res.status(500).json({
-            code:500,
-            message:err,
-            data:null,
-            status:false
+    } catch (err) {
+        return res.status(500).json({
+            code: 500,
+            message: err,
+            data: null,
+            status: false
         })
     }
 })
--- Routes/Auth/registration.js
+++ Routes/Auth/registration.js
@@ -7,55 +7,54 @@ const User = require('../../Model/Auth/user');
 
 const sendEmail = require('../../Middleware/mailer');
 
-router.post('/register',async (req,res)=>{
-    //console.log('inside register');
-    var userexists = await User.findOne({email:req.body.email});
-    if(userexists){
-        res.status(400).json({
-            code:400,
-            message:'Email is already taken!',
-            data:null,
+router.post('/register', async (req, res) => {
+    var userexists = await User.findOne({ email: req.body.email });
+    if (userexists) {
+        return res.status(400).json({
+            code: 400,
+            message: 'Email is already taken!',
+            data: null,
             status: false
         })
     }
     const salt = await crypt.genSalt(10);
-    const hashpassword = await crypt.hash(req.body.password,salt);
+    const hashpassword = await crypt.hash(req.body.password, salt);
 
     const usr = new User({
-        firstname:req.body.firstname,
-        lastname:req.body.lastname,
-        username:req.body.firstname,
-        email:req.body.email,
-        password:hashpassword,
-        phone:req.body.phone,
-        gender:req.body.gender,
-        role:req.body.role
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        username: req.body.firstname,
+        email: req.body.email,
+        password: hashpassword,
+        phone: req.body.phone,
+        gender: req.body.gender,
+        role: req.body.role
     });
 
 
-    try{
+    try {
         var response = await usr.save();
 
         //send welcome email
         let emailOptions = {
             to: req.body.email,
             subject: 'Welcome to Hand 2 Hand community',
-            firstname:req.body.firstname,
-            lastname:req.body.lastname
+            firstname: req.body.firstname,
+            lastname: req.body.lastname
         }
         const emailResponse = sendEmail('welcome', emailOptions);
 
-        res.status(200).json({
-            code:200,
-            message:'Registration Successfull!',
-            data:response,
+        return res.status(200).json({
+            code: 200,
+            message: 'Registration Successfull!',
+            data: response,
             status: true
         })
-    } catch(err) {
-        res.status(500).json({
-            message:err,
-            status:false,
-            code:500,
+    } catch (err) {
+        return res.status(500).json({
+            message: err,
+            status: false,
+            code: 500,
             data: null
         })
     }
